64412b0c1f153baa858c0c693744e1c35a2342bd
Sever-B <omsever@outlook.com>
2024-10-26 15:06:30 +0300

    feature: GET POST PUT DELETE operations for admin page

--- api/alimente.js
+++ api/alimente.js
@@ -1,8 +1,46 @@
 const URL = "https://670fe595a85f4164ef2c616f.mockapi.io/alimente";
 
 export async function getAllProducts() {
-    const response = await fetch(URL);
-    const products =await response.json();
+  const response = await fetch(URL);
+  const products = await response.json();
 
-    return products;
+  return products;
+}
+
+export async function getProductById(id) {
+  const response = await fetch(`${URL}/${id}`);
+  const product = await response.json();
+
+  return product;
+}
+
+export async function addNewProduct(product) {
+  const response = await fetch(URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(product),
+  });
+
+  const newProduct = await response.json();
+  return newProduct;
+}
+
+export async function updateProduct(product, id) {
+  const response = await fetch(`${URL}/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(product),
+  });
+
+  const editedProduct = await response.json();
+
+  return editedProduct;
+}
+
+export async function deleteProduct(id) {
+  await fetch(`${URL}/${id}`, { method: "DELETE" });
 }
